d3c4c4ea3c7a26074aa8b32361a4759d271be94c
Anonoei <to+dev@an0.cx>
2025-08-24 16:26:17 -1000

    bump version 0.0.1 -> 0.1.0

--- src/pyspecan/__init__.py
+++ src/pyspecan/__init__.py
@@ -8,7 +8,7 @@ Github: https://github.com/Anonoei/pyspecan
 PyPI: https://pypi.org/project/pyspecan/
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "Anonoei <to+dev@an0.cx>"
 
 from .config import config
