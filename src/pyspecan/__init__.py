b0404d6e52f24e1a569ad9672f968c19f678a540
Anonoei <to+dev@an0.cx>
2026-01-17 14:57:10 -1000

    bump version 0.5.1 -> 0.5.2

--- src/pyspecan/__init__.py
+++ src/pyspecan/__init__.py
@@ -10,7 +10,7 @@ PyPI: https://pypi.org/project/pyspecan/
 This is intended to be used as a callable module (or a script), but can be used as a library
 """
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __author__ = "Anonoei <to+dev@an0.cx>"
 
 from . import _internal
